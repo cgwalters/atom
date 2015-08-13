6f94706621dce8ccff110febd36639e91dd35f8b
Colin Walters <walters@redhat.com>
2015-08-13 11:29:44 -0400

    Drop dependency on setuptools
    I cannot figure out how to use setuptools only at *build* time.
    There's no reason for us to take a runtime dependency on it.
    
    This is mostly a re-commit of 106878815f5a5dd61b0e527c502fb6eeaf0c9c67
    because which reverts what happened in #112.
    
    It looks like OpenStack uses https://pypi.python.org/pypi/pbr which
    presumably solves this problem.  I however am not super interested
    right now in diving into that...I just want the thing to build.
    
    Ansible just inserts the current dir in `sys.path`, which seems like a
    simple hack, so let's do that.
    
    Conflicts:
    	setup.py

--- Atomic/__init__.py
+++ Atomic/__init__.py
@@ -1,11 +1,8 @@
 import sys
 import os
-from pkg_resources import get_distribution
 from .pulp import PulpServer, PulpConfig
 from .satellite import SatelliteServer, SatelliteConfig
 from .atomic import Atomic
 from .util import writeOut
 
-#https://bitbucket.org/logilab/pylint/issues/36/
-#pylint: disable=no-member
-__version__ = get_distribution('atomic').version
+__version__ = '1.5'
